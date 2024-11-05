b8508056c369bc2e660c8abb98e92c51005e1f01
Samuil <sami_photo@abv.bg>
2024-11-05 17:26:03 +0200

    Added Migration

--- DAL/Entities/Product.cs
+++ DAL/Entities/Product.cs
@@ -7,6 +7,13 @@
         Linux = 3,
         Mobile = 4
     }
+    public enum Rating
+    {
+        Everyone = 1,
+        Teen = 2,
+        Mature = 3,
+        Adult = 4
+    }
 
     public class Product
     {
@@ -16,5 +23,10 @@
         public DateTime DateCreated { get; set; }
         public double TotalRating { get; set; }
         public decimal Price { get; set; }
+        public string Genre { get; set; }
+        public Rating Rating { get; set; }
+        public string? Logo { get; set; }
+        public string? Background { get; set; }
+        public int Count { get; set; }
     }
 }
---
+++ DAL/Migrations/20241105151127_Extend_Products.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class Extend_Products : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_UserAddresses_UserId",
+                table: "UserAddresses");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserAddresses_UserId",
+                table: "UserAddresses",
+                column: "UserId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_UserAddresses_UserId",
+                table: "UserAddresses");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserAddresses_UserId",
+                table: "UserAddresses",
+                column: "UserId");
+        }
+    }
+}
